1379a331c7689466ca47464943ebf09a306aaa1b
BranchManager <nbranch@vols.utk.edu>
2020-06-24 12:34:28 -0500

    format fixes

--- week3/machine-learning-ex2/ex2/PythonFiles/ComputeCost2.py
+++ week3/machine-learning-ex2/ex2/PythonFiles/ComputeCost2.py
@@ -3,6 +3,54 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import scipy.optimize as opt
 
+def sigmoid(z):
+    """
+    Compute sigmoid function given the input z.
+    
+    Parameters
+    ----------
+    z : array_like
+        The input to the sigmoid function. This can be a 1-D vector 
+        or a 2-D matrix. 
+    
+    Returns
+    -------
+    g : array_like
+        The computed sigmoid function. g has the same shape as z, since
+        the sigmoid is computed element-wise on z.
+        
+    Instructions
+    ------------
+    Compute the sigmoid of each value of z (z can be a matrix, vector or scalar).
+    """
+    # convert input to a numpy array
+    z = np.array(z)
+    
+    # You need to return the following variables correctly 
+    g = np.zeros(z.shape)
+
+    # ====================== YOUR CODE HERE ======================
+
+    g = 1/(1+np.exp(-z))
+
+    # =============================================================
+    return g
+    #return 1/(1+np.exp(-z))
+
+#def JofTheta(Theta,exams,admit):
+    
+    #exit()
+
+#def JofTheta(admit,size,z):
+ #   z=np.dot(exams,Theta.T)
+    #print(admit.T)
+   # exit()
+  #  size = admit.size
+    #print(size)
+    #exit()
+   # cost = (1/size)*(np.sum(np.multiply(-admit,np.log(hThetaof(z)))-(np.multiply((1-admit ),np.log(1-hThetaof(z))))))
+    #print(Theta)
+
 def plotData(X, y):
     """
     Plots the data points X and y into a new figure. Plots the data 
@@ -32,6 +80,69 @@ def plotData(X, y):
     plt.show()
     # ============================================================
 
+
+def costFunctionReg(theta, X, y, lambda_):
+    """
+    Compute cost and gradient for logistic regression with regularization.
+    
+    Parameters
+    ----------
+    theta : array_like
+        Logistic regression parameters. A vector with shape (n, ). n is 
+        the number of features including any intercept. If we have mapped
+        our initial features into polynomial features, then n is the total 
+        number of polynomial features. 
+    
+    X : array_like
+        The data set with shape (m x n). m is the number of examples, and
+        n is the number of features (after feature mapping).
+    
+    y : array_like
+        The data labels. A vector with shape (m, ).
+    
+    lambda_ : float
+        The regularization parameter. 
+    
+    Returns
+    -------
+    J : float
+        The computed value for the regularized cost function. 
+    
+    grad : array_like
+        A vector of shape (n, ) which is the gradient of the cost
+        function with respect to theta, at the current values of theta.
+    
+    Instructions
+    ------------
+    Compute the cost `J` of a particular choice of theta.
+    Compute the partial derivatives and set `grad` to the partial
+    derivatives of the cost w.r.t. each parameter in theta.
+    """
+    # Initialize some useful values
+    m = y.size  # number of training examples
+
+    # You need to return the following variables correctly 
+    J = 0
+    grad = np.zeros(theta.shape)
+
+    # ===================== YOUR CODE HERE ======================
+    z=np.dot(X,theta.T)
+    #cost = (1/size)*(np.sum(np.multiply(-admit,np.log(hThetaof(z)))-(np.multiply((1-admit ),np.log(1-hThetaof(z))))))
+    #A = np.multiply(-y,np.log(sigmoid(z)))
+    #E = np.multiply((1-y),np.log(1-sigmoid))
+    #exit()
+
+    B = (np.sum(np.multiply(-y,np.log(sigmoid(z)))-np.multiply((1-y),np.log(1-sigmoid(z)))))
+    J = (1/m)*B+np.multiply((lambda_/(2*m)),(np.square(theta)))
+
+
+    grad = ((1/m)*(np.sum(sigmoid(z)-y)))+((lambda_/m)*theta)
+
+    
+    
+    
+    # =============================================================
+    return J, grad
 
 def mapFeature(X1, X2, degree=6):
     """
--- week3/machine-learning-ex2/ex2/PythonFiles/e2pt2.py
+++ week3/machine-learning-ex2/ex2/PythonFiles/e2pt2.py
@@ -38,3 +38,46 @@ plt.legend(['y = 1', 'y = 0'], loc='upper right')
 
 X = ComputeCost2.mapFeature(X[:,0],X[:,1])
 
+
+
+
+# Initialize fitting parameters
+initial_theta = np.zeros(X.shape[1])
+
+# Set regularization parameter lambda to 1
+# DO NOT use `lambda` as a variable name in python
+# because it is a python keyword
+lambda_ = 1
+
+# Compute and display initial cost and gradient for regularized logistic
+# regression
+cost, grad = ComputeCost2.costFunctionReg(initial_theta, X, y, lambda_)
+
+#print('Cost at initial theta (zeros): {:.3f}'.format(cost))
+print('HERE WE ARE')
+print(list(map('{:.4f}%'.format,cost)))
+
+print('Expected cost (approx)       : 0.693\n')
+
+print('Gradient at initial theta (zeros) - first five values only:')
+print(list(map('{:.4f}%'.format,grad[:5])))
+#print('\t[{:.4f}, {:.4f}, {:.4f}, {:.4f}, {:.4f}]'.format(*grad[:5]))
+
+print('Expected gradients (approx) - first five values only:')
+print('\t[0.0085, 0.0188, 0.0001, 0.0503, 0.0115]\n')
+
+
+# Compute and display cost and gradient
+# with all-ones theta and lambda = 10
+test_theta = np.ones(X.shape[1])
+cost, grad = ComputeCost2.costFunctionReg(test_theta, X, y, 10)
+
+print('------------------------------\n')
+print('Cost at test theta    : {:.2f}'.format(cost))
+print('Expected cost (approx): 3.16\n')
+
+print('Gradient at test theta - first five values only:')
+print('\t[{:.4f}, {:.4f}, {:.4f}, {:.4f}, {:.4f}]'.format(*grad[:5]))
+print('Expected gradients (approx) - first five values only:')
+print('\t[0.3460, 0.1614, 0.1948, 0.2269, 0.0922]')
+
